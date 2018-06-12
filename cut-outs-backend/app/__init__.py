df1f37402c9ad6a266bf24bdd0594979bbbafada
Patrick Dawkins <pjcdawkins@gmail.com>
2018-06-12 16:01:21 +0100

    Form formatting improvements

--- cut-outs-backend/app/__init__.py
+++ cut-outs-backend/app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from app.config import Config
+import os
 
-app = Flask(__name__)
+app_path = os.environ.get('APP_PATH') or '/apps'
+
+app = Flask(
+    __name__,
+    static_url_path=app_path + '/static'
+    )
 
 from app import routes
 
--- cut-outs-backend/app/config.py
+++ cut-outs-backend/app/config.py
@@ -3,5 +3,6 @@ import os
 appRoot = os.environ.get('PLATFORM_APP_DIR') or os.path.dirname(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('PLATFORM_PROJECT_ENTROPY') or 'Developer key'
+    PREFIX = os.environ.get('APP_PATH') or '/apps'
+    SECRET_KEY = os.environ.get('PLATFORM_PROJECT_ENTROPY') or 'development_secret_key'
     SCORE_PATH = os.path.join(appRoot, "resources/scores")
