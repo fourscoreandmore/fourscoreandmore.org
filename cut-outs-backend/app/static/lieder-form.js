b4fec030640743d28b116b251d2546d110cdc7f3
Patrick Dawkins <pjcdawkins@gmail.com>
2018-06-17 20:48:23 +0100

    Always show conditional fields if there are errors

--- cut-outs-backend/app/static/lieder-form.js
+++ cut-outs-backend/app/static/lieder-form.js
@@ -2,18 +2,24 @@ jQuery(document).ready(function( $ ) {
   // Hide the rest length field until preserveRestBars is checked.
   var restLengthField = $('input[name="restLength"]').parent('.form-field');
   if (restLengthField.length > 0) {
-    restLengthField.hide();
     $('input[name="preserveRestBars"]').change(function() {
-      restLengthField.toggle($(this).prop("checked"));
-    });
+      if ($(this).prop("checked") || restLengthField.find('.form-error').length) {
+        restLengthField.show();
+      } else {
+        restLengthField.hide();
+      }
+    }).trigger('change');
   }
 
   // Hide the harmonic rhythm field until the chordHints feature is selected.
   var harmonicRhythmField = $('input[name="harmonicRhythm"]').parent('.form-field');
   if (harmonicRhythmField.length > 0) {
-    harmonicRhythmField.hide();
     $('select[name="addition"]').change(function() {
-      harmonicRhythmField.toggle($(this).val() === "chordHints");
-    });
+      if ($(this).val() === "chordHints" || harmonicRhythmField.find('.form-error').length) {
+        harmonicRhythmField.show();
+      } else {
+        harmonicRhythmField.hide();
+      }
+    }).trigger('change');
   }
 });
