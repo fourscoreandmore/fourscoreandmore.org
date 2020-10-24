edb61b35f3ba9408559ea3c89ac6b5b040541608
Patrick Dawkins <pjcdawkins@gmail.com>
2020-10-24 20:40:54 +0100

    No need to parse lieder, just use the filename

--- cut-outs-backend/app/indexer.py
+++ cut-outs-backend/app/indexer.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import jsonlines
 import logging
-import music21.converter
 import os
 import time
 
@@ -16,48 +15,35 @@ def get_score_name(score):
     return "{} - {}".format(score.metadata.composer, title)
 
 
+def clean_path(path):
+    return str(path).replace("_", " ").strip()
+
+
 def get_lieder_index(path, index_filename="/tmp/lieder.jsonl", rebuild=False):
     fp = open(index_filename, "r")
     existing = {}
-    index = {}
     with jsonlines.Reader(fp) as reader:
         for data in reader.iter(type=dict, skip_invalid=True):
-            if "relative_path" in data:
-                existing[data["relative_path"]] = data
-                index[data["relative_path"]] = data["name"]
+            existing[data["relative_path"]] = data["name"]
 
     if not rebuild:
-        return index
+        return existing
 
-    fp = open(index_filename, "r+")
+    index = {}
+    fp = open(index_filename, "w")
     writer = jsonlines.Writer(fp, flush=True, sort_keys=True)
     for score_path in Path(path).rglob("score.mxl"):
         data = {}
-        start = time.perf_counter()
-        data["abspath"] = os.path.abspath(str(score_path))
         data["relative_path"] = str(score_path.relative_to(path))
-        if data["relative_path"] in existing:
-            existing_score = existing[data["relative_path"]]
-            print("Score already parsed: " + data["relative_path"])
-            data["name"] = existing_score["name"]
-            data["composer"] = existing_score["composer"]
-            data["parse_time"] = existing_score["parse_time"]
-        else:
-            try:
-                score = music21.converter.parse(data["abspath"])
-                data["name"] = get_score_name(score)
-                data["composer"] = score.metadata.composer
-                data["parse_time"] = time.perf_counter() - start
-                print(
-                    f"Parsed score: "
-                    + data["relative_path"]
-                    + f' in {data["parse_time"]:0.4f} seconds'
-                )
-            except Exception as e:
-                logging.exception("Failed to parse score: " + data["abspath"])
-                continue
-            writer.write(data)
+        data["title"] = clean_path(score_path.parent.name)
+        data["collection"] = clean_path(score_path.parent.parent.name)
+        data["composer"] = clean_path(score_path.parent.parent.parent.name)
+        data["name"] = " - ".join(s for s in [data["composer"], data["collection"], data["title"]] if len(s) > 0)
         index[data["relative_path"]] = data["name"]
+        writer.write(data)
+        print("Indexed score: " + data["relative_path"])
+
+    print("Indexed " + str(len(index)) + " scores")
 
     writer.close()
     fp.close()
